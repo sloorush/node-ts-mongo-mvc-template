cea51e93ea56c65df406d003c6a8b54a45cfb0bb
r-ush <aarush.bhatt@gmail.com>
2021-05-01 03:57:17 +0530

    feat: basic user model

---
+++ src/scripts/addUsers.ts
@@ -0,0 +1,16 @@
+import { UserModel } from "../database/users/users.model";
+import { connect, disconnect } from "../database/database";
+
+(async () => {
+  connect();
+  const users = [{ name: "Emma", email: "aarush.bhatt@gmail.com" }];
+  try {
+    for (const user of users) {
+      await UserModel.create(user);
+      console.log(`Created user ${user}`);
+    }
+    disconnect();
+  } catch (e) {
+    console.error(e);
+  }
+})();
